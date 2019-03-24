346b9b31a66c89e9c5e9d4e66c4c3eefd7095e29
Evan Kysley <evan@kysley.com>
2019-03-24 19:31:44 -0400

    feat: routes

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Create from 'Pages/Create'
+import Header from 'Components/Header/Header'
+import { Container } from 'Styled/Container'
+
+const Routes = () => (
+  <Container>
+    <Header />
+    <Switch>
+      <Route path="/create" component={Create} />
+    </Switch>
+  </Container>
+)
+
+export default Routes
